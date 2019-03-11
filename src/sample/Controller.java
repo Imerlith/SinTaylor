5c74ec3d3b2e2bbce5d67cb89d2c8722eca421c8
Imerlith <36335136+Imerlith@users.noreply.github.com>
2019-03-11 23:56:02 +0100

    koncowe przed zajeciami

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,13 +5,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
+
+import static java.lang.Math.PI;
 
 
 public class Controller {
@@ -24,8 +23,7 @@ public class Controller {
 
     @FXML
     private TextField radtextfield;
-    @FXML
-    private Slider radslider;
+
 
     @FXML
     void radbtnOnClick(ActionEvent event){
@@ -38,37 +36,79 @@ public class Controller {
         }
 
     }
+    @FXML
     private boolean validateInput(String input){
-        return input.matches("-?\\d+(\\.\\d+)?/-?\\d+(\\.\\d+)?") || input.matches("-?\\d+(\\.\\d+)?");
+        return input.matches("-?\\d+(\\.\\d+)?");
     }
+
     private void displayResult(){
         if (validateInput(radtextfield.getText())){
-            radResult.setText(radtextfield.getText()+" "+(int)radslider.getValue());
+
+            calculate();
 
         }else {
             radResult.setText("Błędne dane");
         }
         radtextfield.clear();
 
+
     }
     private void calculate(){
-       int numberOfElements = (int)radslider.getValue();
-       String toCalc = radtextfield.getText();
-       double toFind = Double.valueOf(toCalc);
-       //TODO zmienic by sprawdzalo z ktorej karty wyslano i czy podano ulamek z /
-        BigDecimal result = new BigDecimal(toCalc);
-        for (int i = 1; i <=numberOfElements ; i++) {
-            double temp;
-            result = result.add(new BigDecimal())
+        double rad = normalize(Double.valueOf(radtextfield.getText()));
+        int q = quarter(rad);
+        switch (q){
+            case 1:break;
+            case 2:rad = PI-rad;break;
+            case 3:rad=rad-PI;break;
+            case 4:rad = 2.0 * PI - rad;break;
+        }
+        double result= 0;
+        int sign =-1;
+        for (int i = 1; i <11 ; i++) {
+            if(i%2!=0){
+                result +=(-1*sign)*(Math.pow(rad,i)/power(i));
+                sign *=-1;
+                System.out.println(result+"                 err"+deviation(rad,result));
+            }
+            switch (q){
+                case 3:result*=-1;break;
+                case 4:result*=-1;break;
+            }
+            radResult.setText("result:  "+result);
         }
     }
-    private BigInteger factorial(int n){
-        if (n<0){
-            return new BigInteger("0");
-        }else {
+     private long power(int n){
+        if(n<1){
+            return 1;
+        }
+        return n* power(n-1);
+    }
 
-        }
+    private int quarter(double rad){
+        if (rad >= 0.0 && rad < PI / 2.0){
+            return 1;
+        }else  if ( rad >= PI / 2.0 && rad < PI){
+            return 2;
+        }else if( rad >= PI && rad < PI * 3.0 / 2.0){
+            return 3;
+        }else
+            return 4;
     }
+    private double normalize(double rad){
+        double result;
+        double divider = Math.floor(rad/(2.0*PI));
+        if( rad >= 0.0) {
+            result = rad - (divider * 2.0 * PI);
+        } else {
+            result = rad - (divider * 2.0 * PI) + 2.0 * PI;
+        }
+        return result;
+    }
+    private  double deviation(double rad, double result){
+        double correct = Math.sin(rad);
+        return correct-result;
+    }
+
 
 
 
